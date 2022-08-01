dadf7d59bc74b6fa8f9c8a977ed69e0fb34aec46
Rogery Vargas <rogery1999@hotmail.com>
2022-08-01 00:03:38 -0500

    feat: PM2 project setting

--- index.js
+++ index.js
@@ -1,39 +1,20 @@
-const { cpus, platform } = require("os");
-process.env.UV_THREADPOOL_SIZE = 1;
-const cluster = require("cluster");
-//* Only windows
-if (platform() === "win32") {
-  cluster.schedulingPolicy = cluster.SCHED_RR;
-}
-const processMod = require("process");
+const { pbkdf2 } = require("crypto");
+const express = require("express");
 
-if (cluster.isMaster) {
-  const cpusNum = cpus().length;
-  console.log("cpus: ", cpusNum);
-  console.log(`Master Process ${processMod.pid} running`);
+const PORT = 3000;
+const app = express();
 
-  for (let index = 0; index < cpusNum / 2; index++) {
-    cluster.fork();
-  }
-} else {
-  const { pbkdf2 } = require("crypto");
-  const express = require("express");
-
-  const PORT = 3000;
-  const app = express();
-
-  app.get("/", (req, res, next) => {
-    pbkdf2("rogery", "salt", 100000, 512, "sha512", (err, derivedKey) => {
-      if (err) throw Error("Something went wrong");
-      res.json({
-        message: "Home page",
-      });
+app.get("/", (req, res, next) => {
+  pbkdf2("rogery", "salt", 100000, 512, "sha512", (err, derivedKey) => {
+    if (err) throw Error("Something went wrong");
+    res.json({
+      message: "Home page",
     });
   });
+});
 
-  app.get("/fast", (req, res, next) => {
-    res.json("This was fast");
-  });
+app.get("/fast", (req, res, next) => {
+  res.json("This was fast");
+});
 
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-}
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
