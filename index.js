e1dbaf143d4432badc4db7d0e9f92cf3a361153d
Rogery Vargas <rogery1999@hotmail.com>
2022-08-01 00:54:43 -0500

    feat: worker threads

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-const { pbkdf2 } = require("crypto");
+const { Worker } = require("worker_threads");
+const path = require("path");
 const express = require("express");
 
 const PORT = 3000;
 const app = express();
 
 app.get("/", (req, res, next) => {
-  pbkdf2("rogery", "salt", 100000, 512, "sha512", (err, derivedKey) => {
-    if (err) throw Error("Something went wrong");
-    res.json({
-      message: "Home page",
-    });
+  const workerFilePath = path.join(__dirname, "worker.js");
+  const worker = new Worker(workerFilePath);
+
+  worker.on("message", function ({ message, duration }) {
+    res.json({ message: `Worker finish its job in ${duration}` });
   });
+
+  worker.postMessage({ indication: "start" });
 });
 
 app.get("/fast", (req, res, next) => {
